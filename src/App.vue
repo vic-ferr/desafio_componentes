547640559bf4c16259cd7252605c123ec783c773
Victor Fernandez <victorhf2014@gmail.com>
2022-03-15 00:46:42 -0300

    rick&morty

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RickMorty v-for="(comic, i) in comics" :key="i" :animex="comic" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import RickMorty from "@/components/RickMorty";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    RickMorty,
+  },
+  data() {
+    return {
+      comics: [
+        {
+          name: "Rick Sanchez",
+          image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        },
+        {
+          name: "Morty Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        },
+        {
+          name: "Summer Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+        },
+
+        {
+          name: "Beth Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+        },
+        {
+          name: "Jerry Smith",
+          image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+        },
+      ],
+
+      bgImagen:
+        "https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png",
+    };
   },
 };
 </script>
@@ -22,7 +51,21 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #05aa1b;
   margin-top: 60px;
+  background-image: url(https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png);
+  background-position: cover;
+}
+.imagen-rick {
+  border-radius: 50%;
+}
+
+.box-rick-and-morty {
+  display: flex;
+  padding-top: 15px;
+}
+.box-name-h3 {
+  padding: 35px;
+  margin-left: 35px;
 }
 </style>
---
+++ src/components/RickMorty.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="box-general">
+    <div class="col-12 box-rick-and-morty">
+      <img
+        :src="animex.image"
+        :alt="animex.name"
+        width="100"
+        height="100"
+        class="imagen-rick"
+      />
+      <h3 class="box-name-h3">{{ animex.name }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RickMorty",
+  props: {
+    animex: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
